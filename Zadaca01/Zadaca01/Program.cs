36f9e2fd7d2029b7d3a46e29d4cd2645236bf71a
Kristijan Berisa <k_berisa@hotmail.com>
2023-07-05 15:48:57 +0200

    Initial commit

---
+++ Zadaca01/Zadaca01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+//Deklarirajte varijablu tipa int naziva negativniBroj i 
+// dodjelite joj vrijednost -262
+
+// ispišite pozitivni ekvivalent negativniBroj
+
+
+
+// Za unesena dva cijela broja
+// ispišite rezutat djeljenja
+// npr. ulaz 5 i 10, izlaz 0,5 
+
+
+// DZ
+// Za uneseni dvoznamenkasti broj
+// ispišite jediničnu vrijednost
+// unos 21, ispis 1
+// unos 87, ispis 7
+
+
+using System.ComponentModel;
+
+//int negativnibroj = -262;
+//int pozitivni = 262;
+//{
+//    Console.Write(negativnibroj + pozitivni);
+//}
+/*
+int pb = 0;
+int db = 0;
+
+
+Console.Write("Unesi Prvi Broj: ");
+pb =  int.Parse(Console.ReadLine());
+
+Console.Write("Unesi Drugi Broj: ");
+db =  int.Parse(Console.ReadLine());
+
+int zbroj = pb / db;
+
+Console.WriteLine(zbroj);
+*/
+
+int i  = 0;
+int j = 0;
+
+Console.Write("Prvi broj: ");
+i = int.Parse(Console.ReadLine());
+
+Console.Write("Drugi Broj: ");
+j = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(i);
+Console.WriteLine( j);
