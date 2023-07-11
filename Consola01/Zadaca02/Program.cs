1d803b97be845e864cdf8677533eac61cc8be661
Kristijan Berisa <k_berisa@hotmail.com>
2023-07-11 22:47:46 +0200

    update

---
+++ Consola01/Zadaca02/Program.cs
@@ -0,0 +1,128 @@
+﻿//Console.Write("Unesite  broj: ");
+//int broj = int.Parse(Console.ReadLine());
+
+//// algoritam
+//broj = broj + 7;
+
+//// Izlaz
+//Console.WriteLine(broj);
+
+//*********************************************************************
+//int b;
+
+//Console.Write("Unesi broj: ");
+
+//b = int.Parse(Console.ReadLine());
+//if (b < 10)
+//{
+//    Console.WriteLine("Osijek");
+//}
+//else
+
+//    Console.WriteLine("Donji miholjac");
+
+//********************************************************************
+
+//int g;
+//Console.Write("Unesi cijeli broj: ");
+
+//g= int.Parse(Console.ReadLine());
+
+//Console.WriteLine("12: Broj je " + (g % 2 == 0 ? "" : "Ne") + "paran");
+
+//**********************************************************************
+//int pb;
+//int db;
+//Console.Write("Unesi prvi broj: ");
+//pb= int.Parse(Console.ReadLine());
+
+//Console.Write("Unesi drugi broj: ");
+//db= int.Parse(Console.ReadLine());
+
+//int zbroj = pb + db;
+
+//if (zbroj <= 10)
+//{
+//    Console.WriteLine("Edunova");
+//}
+//else
+//{
+//    Console.WriteLine("Osijek");
+//}
+
+//**************************************************************************
+
+//int b1;
+//int b2;
+
+//Console.Write("Unesi prvi broj: ");
+//b1= int.Parse(Console.ReadLine());
+
+//Console.Write("Unesi drugi broj: ");
+//b2 = int.Parse(Console.ReadLine());
+
+//if (b1 > b2)
+//{
+//    Console.WriteLine(b1);
+//}
+//else
+//{
+//    Console.WriteLine(b2);
+//}
+
+//*****************************************************************************
+
+//int b1;
+//int b2;
+//int b3;
+
+//Console.Write("Prvi: ");
+//b1= int.Parse(Console.ReadLine());
+//Console.Write("Drugi: ");
+//b2 = int.Parse(Console.ReadLine());
+//Console.Write("Treci: ");
+//b3 = int.Parse(Console.ReadLine());
+
+
+//if (b1 > b2 && b1> b3)
+//{
+//    Console.WriteLine(b1);
+//}
+//if (b2 > b3 && b2 > b1)
+//{
+//    Console.WriteLine(b2);
+//}
+//else if (b3 > b1 && b3 > b2)
+//{
+//    Console.WriteLine(b3);
+//}
+
+//*********************************************************************************
+
+
+
+
+//{Random i = new Random();
+
+//int r = i.Next(1,999);
+
+//    int zadnji = (r % 10);
+
+//    while (true)
+//    { 
+        
+//        if (r > 999)
+//         {
+//            Console.WriteLine("Greska");
+//            break;
+//         }
+//        else
+//        {
+//            Console.WriteLine(zadnji);
+//            break;
+//        }
+//        }
+//    }
+
+//************************************************************************************
+
